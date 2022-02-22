f8285bf423259275e142202ceea393399b93a805
Tomoki Morita <tmsongbooks215@gmail.com>
2022-02-22 20:04:47 +0900

    Remove laravel version

--- app/Console/Commands/DevHelpCommand.php
+++ app/Console/Commands/DevHelpCommand.php
@@ -17,7 +17,6 @@ class DevHelpCommand extends Command
         $this->table(['Requirements'], [
             ['PHP', '>= 8.0'],
             ['Composer', '2.x'],
-            ['Laravel', '9.x'],
             ['SQLite', '3'],
             ['MailHog', 'https://github.com/mailhog/MailHog'],
         ]);
